c635fa883bd63599fceed20df959832cb39ebd80
Marcin Dancewicz <mdancewicz@gmail.com>
2012-11-22 22:25:28 +0000

    form changed

--- form.php
+++ form.php
@@ -1,12 +1,13 @@
 <?php
-$subject = $_POST['subject'];
-$suggestion = $_POST['suggestion'];
-$name = $_POST['name'];
-$email = $_POST['email'];
+$subject = htmlspecialchars($_POST['subject']);
+$suggestion = htmlspecialchars($_POST['suggestion']);
+$name = htmlspecialchars($_POST['name']);
+$email = htmlspecialchars($_POST['email']);
 $message = 'Name: ' . $name . "<br />";
 $message .= 'Email: ' . $email . "<br />";
 $message .= 'Subject: ' . $subject . "<br />";
 $message .= 'Suggestion: ' . $suggestion . "<br />";
+$message = htmlspecialchars($message);
 
 require_once 'class.phpmailer.php';
 $mail = new PHPMailer(); // defaults to using php "mail()"
@@ -17,9 +18,9 @@ $mail->AddAddress($address, "John Doe");
 $mail->Subject    = "Suggestion from the website";
 $mail->MsgHTML($message);
 if(!$mail->Send()) {
-  echo "Mailer Error: " . $mail->ErrorInfo;
-  echo 'Not sent: <pre>'.print_r(error_get_last(), true).'</pre>';
-  //echo 'There was an error submitting the form, please send us the information to: info@magentostudyguide.com';
+//  echo "Mailer Error: " . $mail->ErrorInfo;
+//  echo 'Not sent: <pre>'.print_r(error_get_last(), true).'</pre>';
+  echo 'There was an error submitting the form, please send us the information to: info@magentostudyguide.com';
 } else {
   echo 'Thank you for sending the suggestion!';
 }
